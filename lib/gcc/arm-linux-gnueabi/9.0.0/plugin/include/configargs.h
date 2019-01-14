5cf50f05ed7ff9a378708ea736b74a2b9e053b34
FacuM <facumo.fm@gmail.com>
2019-01-14 19:14:19 +0000

    Initial commit!

---
+++ lib/gcc/arm-linux-gnueabi/9.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c --target=arm-linux-gnueabi --prefix=/home/zamorano0695/build-tools-gcc/arm-linux-gnueabi";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
